519383c31074ecbfd236be0b0251c7f6490774f9
Kamil Cieślik <kamil.cieslik@protonmail.com>
2018-03-16 03:33:25 +0100

    Building treeView of root subdirectories.

--- src/main/java/javafx/controller/MainController.java
+++ src/main/java/javafx/controller/MainController.java
@@ -4,12 +4,11 @@ package javafx.controller;
 import graphic_files_explorer.Directory;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
+import javafx.stage.DirectoryChooser;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -43,7 +42,14 @@ public class MainController implements Initializable {
 
     @FXML
     void buttonSelectRoot() {
-
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Wybór korzenia drzewa katalogów.");
+        File rootDirectory = chooser.showDialog(scrollPaneImages.getScene().getWindow());
+        if (rootDirectory != null) {
+            TreeItem<Directory> rootDirectoryTreeItem = new TreeItem<>(new Directory(rootDirectory));
+            buildTreeView(rootDirectoryTreeItem);
+            treeViewDirectories.setRoot(rootDirectoryTreeItem);
+        }
     }
 
     @FXML
@@ -69,5 +75,13 @@ public class MainController implements Initializable {
     @FXML
     void treeViewDirectories_onMouseReleased() {
 
+    }
+
+    private void buildTreeView(TreeItem<Directory> treeItem) {
+        treeItem.getValue().getSubdirectories().forEach(dir -> {
+            TreeItem<Directory> directoryTreeItem = new TreeItem<>(new Directory(dir));
+            buildTreeView(directoryTreeItem);
+            treeItem.getChildren().add(directoryTreeItem);
+        });
     }
 }
